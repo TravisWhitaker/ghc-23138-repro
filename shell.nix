d0741649806bbb9cc9a4857587a9fab1127a5eb8
Travis Whitaker <pi.boy.travis@gmail.com>
2023-03-22 13:05:15 -0700

    Still broken.

--- shell.nix
+++ shell.nix
@@ -5,10 +5,8 @@ let pkgsrc = builtins.fetchGit
         rev = "38637c5c8fddf9c5b454918acf309c55e3809af1";
     };
 in with import pkgsrc {};
-let thisghc = haskell.packages.ghc927.ghcWithPackages (p: [p.cabal-install]);
+let thisghc = haskell.packages.ghc944.ghcWithPackages (p: [p.cabal-install]);
 in mkShell
 {
-    packages = [thisghc];
-    #inputsFrom = [ haskell.packages.ghc927.double-conversion
-    #             ];
+    packages = [thisghc git];
 }
